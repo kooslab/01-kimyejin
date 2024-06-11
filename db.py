2c261f156e86fd540c384fe12380938d1e7d5ca8
Jiphyeonjeonsejong <zalal6689@naver.com>
2024-06-11 01:43:14 +0000

    안녕

--- db.py
+++ db.py
@@ -1,50 +1,50 @@
 data = [
     {
         "id": 1,
-        "plant_nm": "깻잎",
-        "family_kor_nm": "깻잎과",
+        "plant_nm": "크림우동",
+        "family_kor_nm": "다연",
         "family_nm": "Brassica",
-        "genus_kor_nm": "깻잎속",
+        "genus_kor_nm": "이",
         "genus_nm": "Brassica",
-        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr8Uh-buvRfMZZXw4LYjN5OZthg6mmEIFsWA&s",
-        "desc": "깻잎은 맛있다"
+        "img_url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20141126_284%2Fcaocao1990_1416989637157o2XQS_PNG%2F250px-574Gothita.png&type=a340",
+        "desc": "다연이의 생일은 언제일까요?"
     },
     {
         "id": 2,
-        "plant_nm": "코코아잎",
-        "family_kor_nm": "코코아잎과",
+        "plant_nm": "안녕나는천세은이고",
+        "family_kor_nm": "세은",
         "family_nm": "Coccaceae",
-        "genus_kor_nm": "코코아속",
+        "genus_kor_nm": "천",
         "genus_nm": "Coccus",
-        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2GJiBJWcs7UlQQDc5ceHK1li4x22AboerzA&s",
-        "desc": "코코아 하하"
+        "img_url": "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fupload2.inven.co.kr%2Fupload%2F2018%2F12%2F12%2Fbbs%2Fi14442197936.jpg%3FMW%3D800&type=a340",
+        "desc": "천태만상"
         },
     {
         "id": 3,
-        "plant_nm": "소나무",
-        "family_kor_nm": "소나무과",
+        "plant_nm": "아",
+        "family_kor_nm": "윤빈",
         "family_nm": "Solanaceae",
-        "genus_kor_nm": "소나무속",
+        "genus_kor_nm": "이",
         "genus_nm": "Solanum",
-        "img_url": "https://i.namu.wiki/i/HPXSQ7eVZsn3oSvNtBItitmeqVnuGq6W-yNzxjsiNAJBgwWM4lr-aIUvr1XEFMVdkEkaOfL4DsesDgaOaZVU-A.webp",
-        "desc": "소나무는 맛있다"
+        "img_url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjAyMjVfMjAx%2FMDAxNjQ1NzYzMjg2MjI2.UxUVQGuEnrykYsZGSIv8w8Ig7XtpVrkoMqSUeeNQGm8g.2mBoo4-8f1s93Ix3RtTL29iL_ShUXePZvYlvatNPd_Qg.PNG.sirius-r%2F%25C6%25F7%25C4%25CF%25B8%25F3%25BD%25BA%25C5%25CD_42%25C8%25AD_%25B4%25EB%25B0%25E1_%25C6%25F7%25C4%25CF%25B8%25F3_%25C3%25BC%25C0%25B0%25B0%25FC.mp4_001068252.png&type=sc960_832",
+        "desc": "미래의간호사"
         },
     {
         "id": 4,
-        "plant_nm": "무궁화",
-        "family_kor_nm": "무궁화과",
+        "plant_nm": "안녕하세요스껄합니다",
+        "family_kor_nm": "효주",
         "family_nm": "Monocotyledon",
-        "genus_kor_nm": "무궁화속",
+        "genus_kor_nm": "사공",
         "genus_nm": "Monocotyledon",
-        "img_url": "https://i.namu.wiki/i/U3xs4ovgGfDKlDujDDa4amcUg9F4xjGKE5X3ZeNc6Jmg1NOxcy6HIJPLCDsNIHDlbgYAcrEui2-Z_H6VsJ921A.webp",
-        "desc": "무궁화는 맛있다"    },
+        "img_url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2F20121120_71%2Fahn3607_1353420091783UWhae_JPEG%2FPokemonAG162_-_%25C6%25F7%25C4%25CF%25B8%25F3_%25C4%25DC%25C5%25D7%25BD%25BA%25C6%25AE_%25C0%25AF%25C1%25EE%25B8%25AE%25C7%25CF_%25B4%25EB%25C8%25B8.avi_000565133.jpg&type=a340",
+        "desc": "라면을 먹자"    },
     {
         "id": 5,
-        "plant_nm": "벚꽅",
-        "family_kor_nm": "벚꽃과",
+        "plant_nm": "엉덩이",
+        "family_kor_nm": "하은",
         "family_nm": "Monocotyledon",
-        "genus_kor_nm": "벚꽃속",
+        "genus_kor_nm": "강",
         "genus_nm": "Monocotyledon",
-        "img_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBCszt6vqLKVVlTYgd4QBy505uM-5gLsoCLA&s",
+        "img_url": "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTA4MDlfMTA1%2FMDAxNjI4NDcyODg3MTI2.obvnOYvXCM9_jM3K3LAgWcpDTb-SgUgXb49NrMzLVd8g.fSM--ldF_Lw5YWGm-stdDXyIpG-bln5E4BRZxZeuCJAg.JPEG.kimjin8946%2F5.jpg&type=a340",
         "desc": "벚꽃 놀이가자"    }
 ]
